22af36b69b3b268d4d209bd55461342d535ecfeb
gabrielomokaro <emekaomokaro54@gmail.com>
2025-02-10 17:59:25 +0100

    update on black jack game

--- blackjack-game/script.js
+++ blackjack-game/script.js
@@ -3,18 +3,28 @@ const head = document.querySelector(".header")
 const firstpar = document.querySelector(".first-par")
 const myCards = document.querySelector(".cards")
 
-// const card2 = document.querySelector(".second-card")
-// const card3 = document.querySelector(".third-card")
 
 let sums = document.querySelector(".sum")
 const button = document.querySelector(".start-btn")
-// let newCard = document.querySelector(".card-new-btn")
+const newCard = document.querySelector(".new-card-btn")
+
 const out  = document.querySelector(".output")
 
-let card1 = Math.floor(Math.random() * 11) + 1
-let card2 = Math.floor(Math.random() * 11) + 1
-let card3 = Math.floor(Math.random() * 11) + 1
-// console.log(card3)
+
+
+
+let card1 = getRandomNumbers();
+let card2 = getRandomNumbers();
+let card3 = getRandomNumbers();
+
+
+function getRandomNumbers() {
+    return Math.floor(Math.random() * 13) + 1;
+}
+
+
+
+
 let sum;
 let hasBlackJack = false;
 let isAlive = true;
@@ -25,55 +35,99 @@ let allCards = [card1, card2];
 
 
 
-// console.log(num1, num2, num3)
-
-button.addEventListener("click", (e) => {
-    // if the sum of all nums is equal to 21, it means blackjack or winner
-    // card1.textContent += " " +  num1;
-    // card2.textContent += " " +  num2;
-    // card3.textContent += " " +  num3;
-
+// function to render a game
+const renderGame = ((e) => {
+    //  if the sum of all nums is equal to 21, it means blackjack or winner
     // allCards.push()
     myCards.textContent += " " + allCards;
-
-
+    
+    
     sum = card1 + card2;
     console.log(sum)
     
     if (sum < 21) {
         firstpar.textContent = " Kindly Draw a Card?"
-        // Event Listener to get a new card
-       document.querySelector(".new-card-btn").addEventListener('click', (e) => {
-           myCards.textContent += "," + card3;
-           sums.textContent = "Sum: " + ""
-           sum += card3;
-           sums.textContent += " " + sum
-           console.log(sum)
-           if (!sum <= 21) {
-            firstpar.textContent = "Draw a Card?"
-            
-           }
-        })
+        firstpar.style.color = "black";
     }else if (sum === 21) {
         hasBlackJack = true;
         firstpar.textContent = "Yoo! You just got BlackJack, Hurray!"
+        firstpar.style.color = "gold";
+
+        if (grandParent) {
+            return grandParent.style.color = "green"
+        }
     }else {
-        firstpar.textContent = "You are out of the game"
+        firstpar.textContent = "You are out of the game!"
+        firstpar.style.color = "red";
         isAlive = false;
     }
-    
 
-
-    
     sums.textContent += " " + sum
     
-
     myCards.textContent;
-    
+    console.log(allCards)
 })
 
-// document.querySelector(".new-card-btn").addEventListener('click', (e) => {
+
+// function to render a new game again
+const newGameRender = ((e) => {
+    allCards.push(card3)
+    myCards.textContent += "," + card3;
     
-//     allCards.push(card3);
-//     sum += card3
-//    })
+
+    sum +=card3;
+    console.log(sum)
+    
+    if (sum < 21) {
+        firstpar.textContent = " Kindly Draw a Card?"
+        firstpar.style.color = "black";
+    }else if (sum === 21) {
+        hasBlackJack = true;
+        firstpar.textContent = "Yoo! You just got BlackJack, Hurray!"
+        firstpar.style.color = "gold";
+        
+        if (grandParent) {
+            grandParent.style.color = "green"
+        }
+        
+    }else {
+        firstpar.textContent = "You are out of the game!!"
+        firstpar.style.color = "red";
+        isAlive = false;
+    }
+
+    sums.textContent = "Sum: " + sum
+
+    console.log(allCards)
+})
+
+
+// function to generate a continous random numbers
+const generate = ((e) => {
+    if (sum !== 21 || sum < 21) {
+        let random = Math.floor(Math.random() * 11) + 1;
+        allCards.push(random);
+        // myCards.textContent += "," + random;
+        sums.textContent = "Sum:" + sum;
+    }
+})
+
+
+
+button.addEventListener("click", (e) => {
+    renderGame();
+
+    newCard.addEventListener("click", (e) => {
+        newGameRender()
+        generate()
+    })
+})
+
+
+// newCard.addEventListener("click", (e) => {
+//     newGameRender()
+//     // generate()
+// })
+
+
+let myArray = ["apple", "banana", "cherry"]  
